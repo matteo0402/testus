107268331058e189433c12c6d634c99121c1a9c5
Matteo Vanggaard Andersen <mva@evercall.dk>
2023-08-24 11:48:36 +0200

    babser

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ROCK NALLE
+          ROCK NALLE ELSKER BABSER
         </p>
         <a
           className="App-link"
